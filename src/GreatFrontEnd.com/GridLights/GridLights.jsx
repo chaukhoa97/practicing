d99e08ae54154d5e225b219e800dac4930d44112
Finn Chau <jonhkane00@gmail.com>
2023-10-30 17:47:00 +0700

    Update GridLights

--- src/GreatFrontEnd.com/GridLights/GridLights.jsx
+++ src/GreatFrontEnd.com/GridLights/GridLights.jsx
@@ -7,36 +7,35 @@ export default function GridLights() {
   const [activeBoxIndexes, setActiveBoxIndexes] = useState([
     2, 3, 5, 7, 1, 8, 6,
   ])
-  const [isReversing, setisReversing] = useState()
-
-  if (activeBoxIndexes.length === 8) {
-    isReversing || setisReversing(true)
-  }
-
-  if (isReversing) {
-    const reversingInterval = setTimeout(() => {
-      const newIndexes = [...activeBoxIndexes]
-      newIndexes.pop()
-      setActiveBoxIndexes(newIndexes)
-    }, 300)
-
-    if (activeBoxIndexes.length === 0) {
-      setisReversing(false)
-      clearTimeout(reversingInterval)
-    }
-  }
 
   const handleBoxClick = (index) => {
-    setActiveBoxIndexes((prev) => {
-      if (!prev.includes(index)) return [...prev, index]
-      return prev
-    })
+    const newIndexes = [...activeBoxIndexes]
+    if (!newIndexes.includes(index)) {
+      newIndexes.push(index)
+    }
+    setActiveBoxIndexes(newIndexes)
+
+    if (newIndexes.length === 8) {
+      const reversingInterval = setInterval(() => {
+        // Use the callback version of setState to ensure we get the most updated value
+        setActiveBoxIndexes((prev) => {
+          const newIndexes = [...prev]
+          newIndexes.pop()
+
+          if (prev.length === 0) {
+            clearInterval(reversingInterval)
+          }
+
+          return newIndexes
+        })
+      }, 100)
+    }
   }
 
   return (
     <div className="grid w-64 grid-cols-3 gap-2">
       {baseArray.map((_, i) => {
-        if (i === 4) return <div key={i} className=""></div>
+        if (i === 4) return <div key={i} />
         return (
           <button
             key={i}
@@ -44,7 +43,7 @@ export default function GridLights() {
               'h-20 w-20 border border-black',
               activeBoxIndexes.includes(i) && 'bg-green-600',
             )}
-            aria-label="grid light box"
+            aria-label={`box ${i}`}
             onClick={() => handleBoxClick(i)}
           ></button>
         )
